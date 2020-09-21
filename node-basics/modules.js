624c6e86c68dd45124f01cc17dd5bc9fbe4a0385
dev-vicki <vignesh.r1999@gmail.com>
2020-09-21 14:46:30 +0530

    finished with modules

--- node-basics/modules.js
+++ node-basics/modules.js
@@ -1,4 +1,11 @@
+const { ages } = require('./people');
+const people = require('./people');
 const xyz = require('./people');
+// const { people, ages } = require("./people");
+// console.log(people, ages);
+console.log(xyz.people, xyz.ages);
+// console.log(people); 
+ 
+const os = require('os');
 
-console.log(xyz);
-// console.log(people);
+console.log(os.platform(), os.homedir());
--- node-basics/people.js
+++ node-basics/people.js
@@ -1,6 +1,11 @@
 const people = ['saran', 'joe', 'alex', 'mario'];
 const ages = [20, 23, 35, 26];
 
-console.log(people);
+// console.log(people);
 
-module.exports = 'people';
+// module.exports = 'people';
+// to access the files or data from other dest we are using export method in node.
+
+module.exports = {
+  people, ages
+}
