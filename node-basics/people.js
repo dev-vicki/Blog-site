52f94d2c7d8d54c8f21267a126903a682abdf5b3
dev-vicki <vignesh.r1999@gmail.com>
2020-09-21 14:21:42 +0530

    node basics part2

---
+++ node-basics/people.js
@@ -0,0 +1,6 @@
+const people = ['saran', 'joe', 'alex', 'mario'];
+const ages = [20, 23, 35, 26];
+
+console.log(people);
+
+module.exports = 'people';
--- node-basics/test.js
+++ node-basics/test.js
@@ -1,22 +1,13 @@
 <<<<<<< HEAD
-// const name = 'vicky';
+const name = 'vicky';
 
-// console.log(name);
-
-const greet = (name) => {
-  console.log(`hello, ${name}`);
-}
-
-greet('pavi');
-=======
-// const name = 'vicky';
-
-// console.log(name);
+console.log(name);
 
 const greet = (name) => {
   console.log(`hello, ${name}`);
 }
 
 greet('pavi');
+
 >>>>>>> 93ac87c... added modules file
 greet('suresh');
