a182eb42fc1b9b5607ea9452d0e852a40b0b50f5
dev-vicki <vignesh.r1999@gmail.com>
2020-09-21 11:39:30 +0530

    basics nodejs part1

---
+++ node-basics/global.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+// global object
+
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("in the timeout msg");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval')
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+=======
+// global object
+
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("in the timeout msg");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval')
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+>>>>>>> 93ac87c... added modules file
+console.log(document.querySelector);
---
+++ node-basics/test.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+// const name = 'vicky';
+
+// console.log(name);
+
+const greet = (name) => {
+  console.log(`hello, ${name}`);
+}
+
+greet('pavi');
+=======
+// const name = 'vicky';
+
+// console.log(name);
+
+const greet = (name) => {
+  console.log(`hello, ${name}`);
+}
+
+greet('pavi');
+>>>>>>> 93ac87c... added modules file
+greet('suresh');
