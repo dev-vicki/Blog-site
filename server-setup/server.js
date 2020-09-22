9b961f81fa4a8881b6ed7eca9837552a0b9d4909
dev-vicki <vignesh.r1999@gmail.com>
2020-09-22 15:13:03 +0530

    dealing with response object in the browser

--- server-setup/server.js
+++ server-setup/server.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  // console.log('request made');
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('content-type', 'text/html');
+
+  res.write('<head><link rel="StyleSheet" href="#"</head>');
+  res.write('<p>hello, ninjas</p>');
+  res.write('<p>wassup? ninjas</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
