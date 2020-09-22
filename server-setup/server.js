83b78d4a1408f9fc364705cdb5707eaefce8c9da
dev-vicki <vignesh.r1999@gmail.com>
2020-09-22 16:41:54 +0530

    done with basic routing, status code and redirecting

--- server-setup/server.js
+++ server-setup/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const { Console } = require('console');
+
 
 const server = http.createServer((req, res) => {
   // console.log('request made');
@@ -14,18 +14,53 @@ const server = http.createServer((req, res) => {
   // res.write('<p>wassup? ninjas</p>');
   // res.end();
 
-      fs.readFile('./views/index.html', (err, data) => {
-        if (err) {
-          Console.log(err);
-          res.end();
-        } else {
-          //res.write(data);
-          res.end(data);
-        }
-      });
+  // sending an html file
+  // fs.readFile(path, (err, data) => {
+  //   if (err) {
+  //     Console.log(err);
+  //     res.end();
+  //   } else {
+  //     //res.write(data);
+  //     res.end(data);
+  //   }
+  // });  
+  
+  // routing
+  let path = './views/';
+    switch(req.url) {
+      case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+      case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+      case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+      default: 
+      path += '404.html';
+      res.statusCode = 404;
+    }
+
+    // send html
+    fs.readFile(path, (err,data) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      }
+      // res.write(data);
+      res.end(data);
+    });
+
 
 });
 
+
+// localhost is the default value for 2nd argument
 server.listen(3000, 'localhost', () => {
   console.log('listening for request on port 3000');
 }); 
